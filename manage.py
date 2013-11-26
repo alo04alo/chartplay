a293aa527c35c939d25750ee9df9e2aaf45d2dd6
alo04alo <sonnv90@gmail.com>
2013-11-26 11:40:45 +0700

    first commit rebase

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
 #!/usr/bin/python
-
+print "SONV - add code to rebase  - 1"
 print "SONNV - add some code"
 print "SONNV - Give a reason why want to request friendship with this user"
