c7ad06b0d8d0821349f60d0befa8c7e57d3405a5
alo04alo <sonnv90@gmail.com>
2013-11-26 11:41:17 +0700

    second commit rebase

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 #!/usr/bin/python
+print "SONNV - add some code to rebase - 2"
 print "SONV - add code to rebase  - 1"
 print "SONNV - add some code"
 print "SONNV - Give a reason why want to request friendship with this user"
